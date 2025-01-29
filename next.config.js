f78a5c035a8e661eb21b2c88970276b626e42d9a
Jackrao-Git <108682585+Jackrao-Git@users.noreply.github.com>
2025-01-29 14:50:33 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,20 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  productionBrowserSourceMaps: false, // enable browser source map generation during the production build
-  // Configure pageExtensions to include md and mdx
+  productionBrowserSourceMaps: false,
   pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
-  experimental: {
-    // appDir: true,
-  },
-  // fix all before production. Now it slow the develop speed.
+  experimental: {},
   eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
   typescript: {
-    // https://nextjs.org/docs/api-reference/next.config.js/ignoring-typescript-errors
     ignoreBuildErrors: true,
+  },
+  i18n: {
+    locales: ['en', 'zh', 'fr'],
+    defaultLocale: 'zh',
   },
 }
 
