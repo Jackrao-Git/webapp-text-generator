ef0b6aecdd6ab77ad6ce70a29e38be7857c18429
Jackrao-Git <108682585+Jackrao-Git@users.noreply.github.com>
2025-01-29 14:19:10 -0500

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -4,12 +4,13 @@ export const API_KEY = `${process.env.NEXT_PUBLIC_APP_KEY}`
 export const API_URL = `${process.env.NEXT_PUBLIC_API_URL}`
 export const IS_WORKFLOW = `${process.env.NEXT_PUBLIC_APP_TYPE_WORKFLOW}` === 'true'
 export const APP_INFO: AppInfo = {
-  title: 'Text Generator APP',
-  description: 'App description',
-  copyright: '',
-  privacy_policy: '',
-  default_language: 'en-US',
+    title: 'My Dify Workflow',
+    description: 'My custom AI automation app',
+    copyright: '',
+    privacy_policy: '',
+    default_language: 'zh-Hans'
 }
+
 
 export const API_PREFIX = '/api'
 
